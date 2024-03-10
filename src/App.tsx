843aa31b2625dc3be576c8508f8c02ab0fb98e89
Artem Rybin <catobazz@gmail.com>
2024-03-10 18:21:55 +0300

    add FlexWrapper.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,8 @@ function App() {
 
 export default App;
 
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
-`;
+// const Title = styled.h1`
+//     font-size: 1.5em;
+//     text-align: center;
+//     color: #e91e63;
+// `;
--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
 import styled from "styled-components";
 import photo from './../../../assets/images/my-proj-ph2.webp'
+import {FlexWrapper} from "../../../components/FlexWrapper";
 
 export const Main = () => {
     return (
-        <div>
-            <div>
-                <span> Hi there </span>
-                <h2> I am Artem Rybin </h2>
-                <h1> FrontEnd Developer </h1>
-            </div>
+        <StyledMain>
 
-            <Photo src={photo} alt='' />
-        </div>
+            <FlexWrapper align={'center'} justify={'space-around'}>
+                <div>
+                    <span> Hi there </span>
+                    <Name> I am Artem Rybin </Name>
+                    <MainTitle> FrontEnd Developer. </MainTitle>
+                </div>
+                <Photo src={photo} alt=''/>
+            </FlexWrapper>
+
+        </StyledMain>
 
     );
 };
+
+const StyledMain = styled.div`
+  min-height: 100vh;
+  background-color: #b6ab76;
+`
 
 const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
+`
+
+const MainTitle = styled.h1`
+ 
+`
+const Name = styled.h2`
+ 
 `
